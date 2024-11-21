f7e211a359339dc835c8004cf0a00e2060a22d2d
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-11-20 22:53:44 -0500

    Add Xcode Project

---
+++ MordaadHomes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MordaadHomes
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MordaadHomes/MordaadHomesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MordaadHomesApp.swift
+//  MordaadHomes
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct MordaadHomesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
